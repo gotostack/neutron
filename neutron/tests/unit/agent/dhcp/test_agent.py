7c97978ae7f868a669f96e5c1f43451d4ecc1fe3
Henry Gessau <gessau@gmail.com>
2016-05-13 21:28:57 -0400

    Remove unhelpful test of oslo.service ServiceLauncher
    Change https://review.openstack.org/315498 was rushed in to fix the
    gate breakage, but reviewers correctly noted that the test does not
    belong in Neutron.
    
    And remove the bgp unhelpful test.
    
    Conflicts:
    	neutron/tests/unit/agent/dhcp/test_agent.py
    
    Change-Id: I25426aae53932459d732ba560733f04d70ea03ac
    (cherry picked from commit 0d837a4be245f6024a6b26727c7c7c0edc6029c5)

--- neutron/tests/unit/agent/dhcp/test_agent.py
+++ neutron/tests/unit/agent/dhcp/test_agent.py
@@ -271,20 +271,6 @@ class TestDhcpAgent(base.BaseTestCase):
                              mock.call().report_state(mock.ANY, mock.ANY,
                                                       mock.ANY)])
 
-    def test_dhcp_agent_main_agent_manager(self):
-        logging_str = 'neutron.agent.common.config.setup_logging'
-        launcher_str = 'oslo_service.service.ServiceLauncher'
-        with mock.patch(logging_str):
-            with mock.patch.object(sys, 'argv') as sys_argv:
-                with mock.patch(launcher_str) as launcher:
-                    sys_argv.return_value = ['dhcp', '--config-file',
-                                             base.etcdir('neutron.conf')]
-                    entry.main()
-                    launcher.assert_has_calls(
-                        [mock.call(cfg.CONF),
-                         mock.call().launch_service(mock.ANY),
-                         mock.call().wait()])
-
     def test_run_completes_single_pass(self):
         with mock.patch(DEVICE_MANAGER):
             dhcp = dhcp_agent.DhcpAgent(HOSTNAME)
