72b327299bffe02d8904ee5e54265302313d43cb
venkata anil <anilvenkata@redhat.com>
2016-10-12 10:57:46 +0000

    Check for ha port to become ACTIVE
    After reboot(restart of l3 and l2 agents) of the node routers
    can be processed by l3 agent before openvswitch agent sets up
    appropriate ha ports. This change add notification for l3 agent
    that ha port becomes ACTIVE and keepalived can be enabled.
    
    note: Release notes added to specify l3 agent dependency on neutron
    server.
    
    Closes-bug: #1597461
    
    Co-Authored-By: venkata anil <anilvenkata@redhat.com>
    
    (cherry picked from commit 25f5912cf8f69f18d111bd60a6cc6ee488755ff3)
    
    Conflicts:
            neutron/db/l3_hascheduler_db.py
            neutron/services/l3_router/l3_router_plugin.py
            neutron/tests/unit/plugins/ml2/test_plugin.py
            neutron/tests/functional/agent/l3/test_ha_router.py
            releasenotes/notes/l3ha-agent-server-dependency-1fcb775328ac4502.yaml
    
    Change-Id: Iedad1ccae45005efaaa74d5571df04197757d07a
    (cherry picked from commit 4ad841c4cf1b23695a792ea6facf1dbf66cb48e9)
    
    split out l3-ha specific test from TestMl2PortsV2
    
    split out test_update_port_status_notify_port_event_after_update
    from ml2.test_plugin.TestMl2PortsV2 into TestMl2PortsV2WithL3
    
    The change set of 25f5912cf8f69f18d111bd60a6cc6ee488755ff3
    change id of Iedad1ccae45005efaaa74d5571df04197757d07a
    introduced a test,
    test_update_port_status_notify_port_event_after_update, that is valid
    only when l3 plugin support l3-ha. Such assumption isn't always true
    depending on actual ml2 driver.
    Since test cases in ml2.test_plugin is used as a common base for
    multiple drivers,
    test_update_port_status_notify_port_event_after_update, may or may not
    pass. So split out tests with very specific assumption into a new
    dedicated testcase so that each driver can safely reuse tests in
    tests/unit/plugin/ml2 based on their characteristics.
    
    Conflicts:
            neutron/tests/unit/plugins/ml2/test_plugin.py
    
    Closes-Bug: #1618601
    Change-Id: Ie81dde976649111d029a7d107c99960aded64915
    (cherry picked from commit 03c412ff011a8d4e86afbada24db675028861728)
    
    Conflicts:
    	neutron/services/l3_router/l3_router_plugin.py
    
    Change-Id: Iedad1ccae45005efaaa74d5571df04197757d07a
    (cherry picked from commit 4ad841c4cf1b23695a792ea6facf1dbf66cb48e9)
    (cherry picked from commit 5860fb21e966ab8f1e011654dd477d7af35f7a27)

--- neutron/services/l3_router/l3_router_plugin.py
+++ neutron/services/l3_router/l3_router_plugin.py
@@ -31,6 +31,7 @@ from neutron.db import l3_db
 from neutron.db import l3_dvr_ha_scheduler_db
 from neutron.db import l3_dvrscheduler_db
 from neutron.db import l3_hamode_db
+from neutron.db import l3_hascheduler_db
 from neutron.db import portforwardings_db
 from neutron.plugins.common import constants
 from neutron.quota import resource_registry
@@ -72,6 +73,8 @@ class L3RouterPlugin(service_base.ServicePluginBase,
         super(L3RouterPlugin, self).__init__()
         if 'dvr' in self.supported_extension_aliases:
             l3_dvrscheduler_db.subscribe()
+        if 'l3-ha' in self.supported_extension_aliases:
+            l3_hascheduler_db.subscribe()
         l3_db.subscribe()
         self.start_rpc_listeners()
 
